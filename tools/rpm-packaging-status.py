b10ed4c2618cefa2d1268e4b6f9370127e25f518
Thomas Bechtold <tbechtold@suse.com>
2016-04-01 09:50:37 +0200

    Add optional check for OBS published XML
    The OpenBuildService has a published XML format with packages and
    versions. Add an optional parameter "--obs-published-xml" which
    points to an xml file. If that parameter is given, also evaluate
    the xml file and output the version from the OBS xml in the tables.
    
    Change-Id: I74d56a538c17192f8132814eaa7f207bf14577e9

--- tools/rpm-packaging-status.py
+++ tools/rpm-packaging-status.py
@@ -25,13 +25,15 @@ import re
 import sys
 import yaml
 
+
 # do some project name corrections if needed
 projects_mapping = {
     "keystoneauth": "keystoneauth1"
 }
 
 
-V = namedtuple('V', ['release', 'rpm_packaging_pkg'])
+V = namedtuple('V', ['release', 'rpm_packaging_pkg',
+                     'obs_published'])
 
 
 def process_args():
@@ -43,6 +45,9 @@ def process_args():
     parser.add_argument('rpm-packaging-git-dir',
                         help='Base directory of the openstack/rpm-packaging '
                         'git repo', default='rpm-packaging')
+    parser.add_argument('--obs-published-xml',
+                        help='path to a published xml file from the '
+                        'openbuildservice')
     parser.add_argument('release',
                         help='name of the release. I.e. "mitaka"',
                         default='mitaka')
@@ -61,6 +66,29 @@ def find_highest_release_version(releases):
     return max(version.parse(r['version']) for r in releases)
 
 
+def find_openbuildservice_pkg_version(published_xml, pkg_name):
+    """find the version in the openbuildservice published xml for the given
+    pkg name"""
+    import pymod2pkg
+    from rpmUtils.miscutils import splitFilename
+    import xml.etree.ElementTree as ET
+
+    if published_xml and os.path.exists(published_xml):
+        with open(published_xml) as f:
+            tree = ET.fromstring(f.read())
+
+        distro_pkg_name = pymod2pkg.module2package(pkg_name, 'suse')
+        for child in tree:
+            if not child.attrib['name'].startswith('_') and \
+               child.attrib['name'].endswith('.rpm') and not \
+               child.attrib['name'].endswith('.src.rpm'):
+                (name, ver, release, epoch, arch) = splitFilename(
+                child.attrib['name'])
+                if name == distro_pkg_name:
+                    return version.parse(ver)
+    return version.parse('0')
+
+
 def find_rpm_packaging_pkg_version(pkg_project_spec):
     """get a spec.j2 template and get the version"""
     if os.path.exists(pkg_project_spec):
@@ -75,11 +103,16 @@ def find_rpm_packaging_pkg_version(pkg_project_spec):
     return version.parse('0')
 
 
-def _pretty_table(release, projects):
+def _pretty_table(release, projects, include_obs):
     from prettytable import PrettyTable
     tb = PrettyTable()
-    tb.field_names = ['name', 'release (%s)' % release,
-                      'rpm packaging (%s)' % release, 'comment']
+    fn = ['name', 'release (%s)' % release,
+          'rpm packaging (%s)' % release]
+    if include_obs:
+        fn += ['obs']
+    fn += ['comment']
+    tb.field_names = fn
+
     for p_name, x in projects.items():
         if x.rpm_packaging_pkg == version.parse('0'):
             comment = 'needs packaging'
@@ -91,19 +124,25 @@ def _pretty_table(release, projects):
             comment = 'needs downgrade'
         else:
             comment = ''
-        tb.add_row([p_name, x.release, x.rpm_packaging_pkg, comment])
+        row = [p_name, x.release, x.rpm_packaging_pkg]
+        if include_obs:
+            row += [x.obs_published]
+        row += [comment]
+
+        tb.add_row(row)
+
     return tb
 
 
-def output_text(release, projects):
-    tb = _pretty_table(release, projects)
+def output_text(release, projects, include_obs):
+    tb = _pretty_table(release, projects, include_obs)
     print(tb.get_string(sortby='name'))
 
 
-def output_html(release, projects):
+def output_html(release, projects, include_obs):
     """adjust the comment color a big with an ugly hack"""
     from lxml import html
-    tb = _pretty_table(release, projects)
+    tb = _pretty_table(release, projects, include_obs)
     s = tb.get_html_string(sortby='name')
     tree = html.document_fromstring(s)
     tab = tree.cssselect('table')
@@ -155,13 +194,20 @@ def main():
             '%s.spec.j2' % project_name_pkg)
         v_rpm_packaging_pkg = find_rpm_packaging_pkg_version(rpm_packaging_pkg_project_spec)
 
-        # add both versions to the project dict
-        projects[project_name] = V(v_release, v_rpm_packaging_pkg)
+        # version from build service published file
+        v_obs_published = find_openbuildservice_pkg_version(
+            args['obs_published_xml'], project_name)
 
+        # add both versions to the project dict
+        projects[project_name] = V(v_release,
+                                   v_rpm_packaging_pkg,
+                                   v_obs_published)
+
+    include_obs = args['obs_published_xml']
     if args['format'] == 'text':
-        output_text(args['release'], projects)
+        output_text(args['release'], projects, include_obs)
     elif args['format'] == 'html':
-        output_html(args['release'], projects)
+        output_html(args['release'], projects, include_obs)
 
     return 0
 
